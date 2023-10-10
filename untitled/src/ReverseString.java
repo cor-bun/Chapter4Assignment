69008a3e731d56022239fc243183f7eff738874d
Corbin <cahendricks@live.com>
2023-10-10 18:36:06 -0400

    Exercise 8 Completed

--- untitled/src/ReverseString.java
+++ untitled/src/ReverseString.java
@@ -9,6 +9,13 @@
 import java.util.Scanner;
 public class ReverseString {
     public static void main(String[] args) {
+        Scanner inputOne = new Scanner(System.in);
+        System.out.println("Please enter a string of characters.");
+        String chars = inputOne.nextLine();
+        char[] reverse = new char[chars.length()];
 
+        for(int i = reverse.length - 1; i >= 0; i--) {
+            System.out.print(chars.charAt(i));
+        }
     }
 }
