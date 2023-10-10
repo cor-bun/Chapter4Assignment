5c7415fa87d1f2eba3dbb4f1a12d3fb1fa494ab2
Corbin <cahendricks@live.com>
2023-10-10 09:50:12 -0400

    Second Exercise Complete

--- untitled/src/GradeAverage.java
+++ untitled/src/GradeAverage.java
@@ -2,13 +2,29 @@
  * @author Corbin Hendricks
  * @version 1.0
  * Course: ITEC 2140 Fall 2023
- * Written: October 9, 2023
+ * Written: October 10, 2023
  * This class requests the number of students, those students' grades, then produces the average grade in the class.
  */
 
+import java.util.Arrays;
 import java.util.Scanner;
 public class GradeAverage {
     public static void main(String[] args) {
+        Scanner inputOne = new Scanner(System.in);
+        System.out.println("How many students are in the class?");
+        int classSize = inputOne.nextInt();
+
+        System.out.println("Please enter the students' grades.");
+        int[] classGrades = new int[classSize];
+        int gradesSum = 0;
+        for(int i = 0; i < classSize; i++) {
+            Scanner inputTwo = new Scanner(System.in);
+            classGrades[i] = inputTwo.nextInt();
+            gradesSum = gradesSum + classGrades[i];
+        }
+        System.out.println("Class grades are...");
+        System.out.println(Arrays.toString(classGrades));
+        System.out.println("Class average is " + gradesSum / classSize + ".");
 
     }
 }
